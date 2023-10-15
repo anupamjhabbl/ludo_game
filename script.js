b74dd0d9c5c8a99d52d6e9ee0f96dd3292c67b54
anupamjhabbl <anupamimpbbl@gmail.com>
2023-10-16 02:01:52 +0530

    added data structures to contain track of elements and also add functionality of blinking

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // putting all the gotis in their home in starting
+
 let yellow_gotis = document.getElementsByClassName('peeli_goti');
 for (let i=0;i<yellow_gotis.length;i++){
     let yellow_goti = document.createElement('div');
     yellow_goti.className = 'js_yellow_goti';
+    yellow_goti.id = `id_yellow_goti${i+1}`;
     yellow_gotis[i].appendChild(yellow_goti);
 }
 
@@ -10,6 +12,7 @@ let blue_gotis = document.getElementsByClassName('nili_goti');
 for (let i=0;i<blue_gotis.length;i++){
     let blue_goti = document.createElement('div');
     blue_goti.className = 'js_blue_goti';
+    blue_goti.id = `id_blue_goti${i+1}`;
     blue_gotis[i].appendChild(blue_goti);
 }
 
@@ -17,6 +20,7 @@ let green_gotis = document.getElementsByClassName('hari_goti');
 for (let i=0;i<green_gotis.length;i++){
     let green_goti = document.createElement('div');
     green_goti.className = 'js_green_goti';
+    green_goti.id = `id_green_goti${i+1}`;
     green_gotis[i].appendChild(green_goti);
 }
 
@@ -24,12 +28,30 @@ let red_gotis = document.getElementsByClassName('laal_goti');
 for (let i=0;i<red_gotis.length;i++){
     let red_goti = document.createElement('div');
     red_goti.className = 'js_red_goti';
+    red_goti.id = `id_red_goti${i+1}`;
     red_gotis[i].appendChild(red_goti);
 }
 
+
+
+// mapping every color with array containing position of their gotis
+let gotis_position_array_map = {
+    "red":[-1,-1,-1,-1],
+    "yellow":[-1,-1,-1,-1],
+    "green":[-1,-1,-1,-1],
+    "blue":[-1,-1,-1,-1]
+}
+
+let active_status = new Map();
+const color_map = new Map([
+    [0,"red"],
+    [1,"blue"],
+    [2,"yellow"],
+    [3,"green"]
+])
+
 // rshowing the chance function
 let change_the_chance = (chance) => {
-    console.log(chance);
     let chance_goti = document.getElementById('chance_goti');
     switch(chance){
         case 0:
@@ -73,13 +95,50 @@ roll_button.onclick = () => {
     user_move(uservalue, chance);
 
     if (uservalue!=6){
-        console.log("helo world");
         chance = (chance+1)%4;
         change_the_chance(chance);
     }
 }
 
 let user_move = (uservalue, chance) => {
-    console.log(chance+" got the number "+uservalue);
+    if (uservalue==6){
+        for (let i=1;i<=4;i++){
+            active_status.set(`id_${color_map.get(chance)}_goti${i}`,1);
+            document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className += " blink";
+        }
+    }
+    else{
+        for (let i=1;i<=4;i++){
+            if (gotis_position_array_map[color_map.get(chance)][i-1]!=-1){
+                active_status.set(`id_${color_map.get(chance)}_goti${i}`,1);
+                document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className += " blink";
+            }
+        }
+    }
+
+    move_the_goti(uservalue);
+
+    if (uservalue==6){
+        for (let i=1;i<=4;i++){
+            active_status.set(`id_${color_map.get(chance)}_goti${i}`,0);
+            let k = document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className;
+            document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className = k.substring(0,k.length-6);
+        }
+    }
+    else{
+        for (let i=1;i<=4;i++){
+            if (gotis_position_array_map[color_map.get(chance)][i-1]!=-1){
+                active_status.set(`id_${color_map.get(chance)}_goti${i}`,0);
+                let k = document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className;
+                document.getElementById(`id_${color_map.get(chance)}_goti${i}`).className = k.substring(0,k.length-6);
+            }
+        }
+    }
+
+}
+
+// function to move the goti
+const move_the_goti = (uservalue) => {
+    
 }
 
